0e5ef7bda46b018b5574ebe50df34261be6ee424
Shoyayeb <sa.fine@outlook.com>
2022-05-20 20:14:38 +0600

    fixed mongodb not connecting and api error

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,17 +1,16 @@
 const { MongoClient } = require("mongodb");
 
-
+const uri = process.env.MONGODB_URI;
+const options = {}
 export default async function handler(req, res) {
-    const uri = `mongodb+srv://<${process.env.DB_USERNAME}>:<${process.env.DB_PASSWORD}>@cluster0.4f4qc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+    if (req.method != 'POST') {
+        return res.status(405).send({ message: 'Method not allowed', status: '405' })
+    }
     // mongodb config
-    const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    client.connect();
-    const database = client.db("productData");
-    const productCollection = database.collection("products");
-    if (req.method === 'POST') {
+    const client = new MongoClient(uri, options)
+    client.connect().then((result) => {
+        const database = client.db("productData");
+        const productCollection = database.collection("products");
         console.log('post', req.body);
         const { name, description, price } = req.body;
         const productData = {
@@ -21,15 +20,17 @@ export default async function handler(req, res) {
         }
         productCollection.insertOne(productData).then((result) => {
             console.log(result);
-            res.status(200).send({ message: result, status: '200' })
+            return res.status(200).send({ message: result, status: '200' });
         }).catch((err) => {
             console.log(err);
-            res.status(500).send({
+            return res.status(500).send({
                 message: 'The server has encountered a error adding the product.', status: '500'
-            })
+            });
         });
-    }
-    else {
-        res.status(405).send({ message: 'Method not allowed', status: '405' })
-    }
+    }).catch((err) => {
+        console.log(err);
+        return res.status(500).send({
+            message: 'The server has encountered a error adding the product.', status: '500'
+        });
+    });
 }
